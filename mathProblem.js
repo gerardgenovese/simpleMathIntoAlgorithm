eb5319501642b5225093da60af2432dd1b27a88e
Gerard <gerardgenovese24@gmail.com>
2017-11-08 13:31:12 -0500

    Add files via upload

---
+++ mathProblem.js
@@ -0,0 +1,23 @@
+
+// There were some people on a train.
+// 19 people get off the train at the first stop. 
+// 17 people get on the train.
+// Now there are 63 people on the train.
+// How many people were on the train to begin with?
+
+var test = function() { 
+    for (var i = 0; i < 100; i++) { 
+        if (i - 19 + 17 === 63) { 
+        var answer = i; 
+        console.log(answer); 
+        } 
+    } 
+} 
+test();
+
+
+    
+
+   
+  
+     
